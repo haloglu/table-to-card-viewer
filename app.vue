7fc5c12f7d1add984857645b108ba1a60355d8ba
haloglu <haloglusertac@gmail.com>
2025-05-30 01:17:31 +0300

    Splash screen added app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <!-- app.vue -->
 <template>
   <div>
+    <Head>
+      <!-- Apple Splash için örnek çözünürlük -->
+      <link
+        rel="apple-touch-startup-image"
+        href="/apple-splash-1242-2688.jpg"
+        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+      />
+      <!-- Gerekirse diğer çözünürlükleri de eklersin -->
+    </Head>
+
     <ThemeToggle />
     <NuxtPage />
   </div>
